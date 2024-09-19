989772078961099aea88180d1af1878b533ae986
Jose Luis <wildchamo@gmail.com>
2024-09-19 12:32:17 -0500

    manijauto

--- services/submit-image/index.js
+++ services/submit-image/index.js
@@ -1,5 +1,7 @@
 import AWS from "aws-sdk";
 
+import * as XLSX from "xlsx";
+
 const awsAccessKey = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
 const awsSecretKey = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY;
 const awsRegion = process.env.NEXT_PUBLIC_AWS_REGION;
@@ -17,7 +19,7 @@ function sanitizeFileName(fileName) {
     .replace(/[^a-z0-9_\-\.]/g, "");
 }
 
-export async function uploadFile(file) {
+export async function uploadFile(file, folderName = "manijauto") {
   const originalFileName = file.name;
   const sanitizedFileName = sanitizeFileName(
     originalFileName.split(".").slice(0, -1).join(".")
@@ -30,9 +32,11 @@ export async function uploadFile(file) {
 
   const newFileName = `${sanitizedFileName}_${randomNumber}.${fileExtension}`;
 
+  const key = `${folderName}/${newFileName}`;
+
   const params = {
     Bucket: "refaccionesdotcom",
-    Key: newFileName,
+    Key: key,
     Body: file,
     ContentType: file.type,
   };
@@ -45,4 +49,57 @@ export async function uploadFile(file) {
     console.error("Error uploading file: ", error);
     throw error;
   }
+}
+
+async function listAllObjects(
+  bucketName,
+  continuationToken = null,
+  allObjects = []
+) {
+  const params = {
+    Bucket: bucketName,
+    ContinuationToken: continuationToken,
+  };
+
+  try {
+    const data = await s3.listObjectsV2(params).promise();
+    allObjects.push(...data.Contents);
+
+    console.log("Objects listed: ", data.Contents.length);
+
+    if (data.IsTruncated) {
+      return listAllObjects(bucketName, data.NextContinuationToken, allObjects);
+    } else {
+      return allObjects;
+    }
+  } catch (error) {
+    console.error("Error listing objects: ", error);
+    throw error;
+  }
+}
+
+async function generateExcelFromBucket(bucketName) {
+  console.warn("arrancando");
+  try {
+    const objects = await listAllObjects(bucketName);
+    const data = objects.map((obj) => ({
+      name: getFileNameWithoutExtension(obj.Key),
+      url: `https://${bucketName}.s3.${awsRegion}.amazonaws.com/${obj.Key}`,
+    }));
+
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Files");
+    XLSX.writeFile(workbook, "bucket_files.xlsx");
+
+    console.log("Excel file generated successfully.");
+  } catch (error) {
+    console.error("Error generating Excel file: ", error);
+  }
+}
+
+// generateExcelFromBucket("refaccionesdotcom");
+
+function getFileNameWithoutExtension(fileName) {
+  return fileName.split(".").slice(0, -1).join(".");
 }
